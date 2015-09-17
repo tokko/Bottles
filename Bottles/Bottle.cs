865661a8b2bf0ffdbfbf0b669bd55e1c41177d73
tokko <andreg@kth.se>
2015-09-17 21:41:56 +0200

    new pourinto with test

--- Bottles/Bottle.cs
+++ Bottles/Bottle.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Bottles
 {
+	[DebuggerDisplay("Volume: {Volume}")]
     public class Bottle
     {
 	    public int Capacity { get; set; }
@@ -18,39 +21,86 @@ namespace Bottles
 		    Volume = 0;
 	    }
 
+		public bool IsEmpty()
+		{
+			return Volume == 0;
+		}
+
+		public bool IsFull()
+		{
+			return Volume == Capacity;
+		}
 	    public void PourInto(Bottle b)
 	    {
-			b.Volume = (b.Volume + Volume);
-			if (b.Volume > b.Capacity)
-				b.Volume = b.Capacity;
-			Volume = b.Volume % b.Capacity;
-			
+		    var free = b.Capacity - b.Volume;
+		    if (free >= Volume)
+		    {
+			    b.Volume += Volume;
+			    Volume = 0;
+			    return;
+		    }
+		    b.Volume = b.Capacity;
+		    Volume -= free;
 	    }
 	}
 
 	public class BottleSolver
 	{
-		public string Solve(int capacityBottleA, int capacityBottleB, int desiredVolume)
+		public static List<string> Solve(int capacityBottleA, int capacityBottleB, int desiredVolume)
 		{
 			var bottleA = new Bottle {Capacity = capacityBottleA};
 			var bottleB = new Bottle {Capacity = capacityBottleB};
 
 			var solution = string.Empty;
 
-			return string.Empty;
+			return Solve(solution, 0, 8, bottleA, bottleB, desiredVolume);
 		}
 
-		private string Solve(string path, int depth, int limit, Bottle bottleA, Bottle bottleB, int desiredVolume)
+		private static List<string> Solve(string path, int depth, int limit, Bottle bottleA, Bottle bottleB, int desiredVolume)
 		{
-			if (bottleA.Volume == desiredVolume) return " A;";
-			if (bottleB.Volume == desiredVolume) return " B;";
-			if (depth > limit) return string.Empty;
+			if (bottleA.Volume == desiredVolume) return new List<string> {path + "A;"};
+			if (bottleB.Volume == desiredVolume) return new List<string> {path + "B;"};
+			if (depth > limit) return new List<string>{path + ";"};
 			var actions = new Dictionary<string, Action>
 			{
-				//{"a -> b" : () => bottleB.},
+				{"A -> B ", () => bottleA.PourInto(bottleB)},
+				{"B -> A ", () => bottleB.PourInto(bottleA)},
+				{"E(A) ", bottleA.Empty},
+				{"E(B) ", bottleB.Empty},
+				{"F(A) ", bottleA.Fill},
+				{"F(B) ", bottleB.Fill},
 			};
+			PruneActions(bottleA, bottleB, actions);
+			return actions.Select(action =>
+			{
+				action.Value();
+				var s = Solve(path + action.Key, depth+1, limit, bottleA, bottleB, desiredVolume);
+				return s;
+			}).Aggregate(new List<string>(), (x, y) => x.Concat(y).ToList()).Where(s => s.EndsWith("B;") || s.EndsWith("A;")).ToList();
+		}
 
-			return string.Empty;
+		private static void PruneActions(Bottle bottleA, Bottle bottleB, Dictionary<string, Action> actions)
+		{
+			if (bottleA.IsEmpty())
+			{
+				actions.Remove("A -> B ");
+				actions.Remove("E(A) ");
+			}
+			if (bottleB.IsEmpty())
+			{
+				actions.Remove("B -> A ");
+				actions.Remove("E(B) ");
+			}
+			if (bottleA.IsFull())
+			{
+				actions.Remove("B -> A ");
+				actions.Remove("F(A) ");
+			}
+			if (bottleB.IsFull())
+			{
+				actions.Remove("A -> B ");
+				actions.Remove("F(B) ");
+			}
 		}
 	}
 }
--- BottlesTest/BottlesTest.cs
+++ BottlesTest/BottlesTest.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using Bottles;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
 
 namespace BottlesTest
 {
-	[TestClass]
+	[TestFixture]
 	public class BottlesTest
 	{
-		[TestMethod]
+		[Test]
 		public void PourFrom_LargeToSmallNoOverflow_VolumeIsCorrect()
 		{
 			var bottleA = new Bottle {Capacity = 5, Volume = 3};
@@ -20,5 +19,30 @@ namespace BottlesTest
 			Assert.That(bottleA.Volume, Is.EqualTo(0));
 			Assert.That(bottleB.Volume, Is.EqualTo(3));
 		}
+
+		[Test]
+		public void PourFrom_LargeToSmallOverflow_VolumeIsCorrect()
+		{
+			var bottleA = new Bottle {Capacity = 5, Volume = 5};
+			var bottleB = new Bottle {Capacity = 3};
+
+			bottleA.PourInto(bottleB);
+
+			Assert.That(bottleA.Volume, Is.EqualTo(2));
+			Assert.That(bottleB.Volume, Is.EqualTo(3));
+		}
+
+		[Test]
+		[Ignore]
+		public void Solve_3And5LitreContainers4LitreDesired_ReturnsCorrectPaths()
+		{
+			var solution = BottleSolver.Solve(5, 3, 1);
+
+			Assert.NotNull(solution);
+			Assert.True(solution.Count > 0);
+		}
 	}
+
+
+
 }
