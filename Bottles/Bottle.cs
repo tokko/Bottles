2c666cae344ddb3d9b34908c1acbfb8f9a322c97
tokko <andreg@kth.se>
2015-09-17 20:30:12 +0200

    initial commit

---
+++ Bottles/Bottle.cs
@@ -0,0 +1,39 @@
+﻿namespace Bottles
+{
+    public class Bottle
+    {
+	    public int Capacity { get; set; }
+	    public int Volume { get; set; }
+
+	    public void Fill()
+	    {
+		    Volume = Capacity;
+	    }
+
+	    public void Empty()
+	    {
+		    Volume = 0;
+	    }
+
+	    public void PourFrom(Bottle b)
+	    {
+		    Volume = (Volume + b.Volume);
+		    if (Volume > Capacity)
+			    Volume = Capacity;
+			b.Volume = Volume % Capacity;
+			
+	    }
+	}
+
+	public class BottleSolver
+	{
+		public string Solve(int capacityBottleA, int capacityBottleB, int desiredVolume)
+		{
+			var bottleA = new Bottle {Capacity = capacityBottleA};
+			var bottleB = new Bottle {Capacity = capacityBottleB};
+
+
+			return string.Empty;
+		}
+	}
+}
---
+++ BottlesTest/BottlesTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Bottles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace BottlesTest
+{
+	[TestClass]
+	public class BottlesTest
+	{
+		[TestMethod]
+		public void PourFrom_LargeToSmallNoOverflow_VolumeIsCorrect()
+		{
+			var bottleA = new Bottle {Capacity = 5, Volume = 3};
+			var bottleB = new Bottle {Capacity = 3};
+
+			bottleB.PourFrom(bottleA);
+
+			Assert.That(bottleA.Volume, Is.EqualTo(0));
+			Assert.That(bottleB.Volume, Is.EqualTo(3));
+		}
+	}
+}
